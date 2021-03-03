16018f9666a5fcfa7c6e32710b357cec4eb7e129
Karel Vanhoorebeeck <karelvh@gmail.com>
2021-03-03 09:17:57 +0100

    Adding basic api wrappers

--- raymon/loggers.py
+++ raymon/loggers.py
@@ -68,10 +68,6 @@ class RaymonLoggerBase(ABC):
     def tag(self, ray_id, tags):
         pass
 
-    @abstractmethod
-    def flush(self):
-        pass
-
     """
     Functions related to Authentication
     """
@@ -79,54 +75,6 @@ class RaymonLoggerBase(ABC):
     def login(self, fpath):
         self.token = login(fpath=fpath)
         self.headers["Authorization"] = f"Bearer {self.token}"
-
-
-class RaymonAPI(RaymonLoggerBase):
-    def __init__(self, url="http://localhost:8000", project_id="default", auth_path=None):
-        super().__init__(project_id=project_id, auth_path=auth_path)
-        self.url = url
-        self.session = requests.Session()
-        # self.secret = load_secret(project_name=project_id, fpath=secret_fpath)
-
-    """
-    Functions related to logging of rays
-    """
-
-    def info(self, ray_id, text):
-        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
-        jcr = self.structure(ray_id=ray_id, peephole=None, data=text)
-        self.logger.info(text, extra=jcr)
-        resp = self.post(route=f"projects/{self.project_id}/ingest", data=jcr)
-        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
-        self.logger.info(f"Logged info. Status: {status}", extra=jcr)
-
-    def log(self, ray_id, peephole, data):
-        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
-        jcr = self.structure(ray_id=ray_id, peephole=peephole, data=data.to_jcr())
-        self.logger.info(f"Logging data at {peephole}", extra=jcr)
-        resp = self.post(route=f"projects/{self.project_id}/ingest", data=jcr)
-        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
-        self.logger.info(f"Data logged at {peephole}. Status: {status}", extra=jcr)
-
-    def tag(self, ray_id, tags):
-        # TODO validate tags
-        jcr = self.structure(ray_id=ray_id, peephole=None, data=tags)
-        resp = self.post(route=f"projects/{self.project_id}/rays/{ray_id}/tags", data=tags)
-        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
-        self.logger.info(f"Ray tagged. Status: {status}", extra=jcr)
-
-    def post(self, route, data):
-        resp = self.session.post(f"{self.url}/{route}", json=data, headers=self.headers)
-        return resp
-
-    def get(self, route, params):
-        resp = self.session.get(f"{self.url}/{route}", params=params, headers=self.headers)
-
-        return resp
-
-    def flush(self):
-        # We don't need to do anything here
-        pass
 
 
 class RaymonTextFile(RaymonLoggerBase):
@@ -153,7 +101,6 @@ class RaymonTextFile(RaymonLoggerBase):
         return logger
 
     def info(self, ray_id, text):
-        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
         jcr = self.structure(ray_id=ray_id, peephole=None, data=text)
         kafka_msg = {"type": "info", "jcr": jcr}
         self.logger.info(text, extra=jcr)
@@ -161,22 +108,147 @@ class RaymonTextFile(RaymonLoggerBase):
         self.logger.info(f"Logged: {text}", extra=jcr)
 
     def log(self, ray_id, peephole, data):
-        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
         jcr = self.structure(ray_id=ray_id, peephole=peephole, data=data.to_jcr())
         kafka_msg = {"type": "data", "jcr": jcr}
         self.logger.info(f"Logging data at {peephole}", extra=jcr)
-        # resp = requests.post(f"{self.url}/projects/{self.project_id}/ingest", json=jcr, headers=self.headers)
         self.data_logger.info(json.dumps(kafka_msg))
         self.logger.info(f"Logged peephole {peephole} data to textfile.", extra=jcr)
 
     def tag(self, ray_id, tags):
-        # TODO validate tags
         jcr = self.structure(ray_id=ray_id, peephole=None, data=tags)
         kafka_msg = {"type": "tags", "jcr": jcr}
-        # resp = requests.post(f"{self.url}/projects/{self.project_id}/rays/{ray_id}/tags", json=tags, headers=self.headers)
         self.data_logger.info(json.dumps(kafka_msg))
         self.logger.info(f"Logged tags to textfile.", extra=jcr)
 
-    def flush(self):
-        # We don't need to do anything here
-        pass
+
+class RaymonAPI(RaymonLoggerBase):
+    def __init__(self, url="http://localhost:8000", project_id="default", auth_path=None):
+        super().__init__(project_id=project_id, auth_path=auth_path)
+        self.url = url
+        self.session = requests.Session()
+        # self.secret = load_secret(project_name=project_id, fpath=secret_fpath)
+
+    """
+    Functions related to logging of rays
+    """
+
+    def info(self, ray_id, text):
+        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
+        jcr = self.structure(ray_id=ray_id, peephole=None, data=text)
+        self.logger.info(text, extra=jcr)
+        resp = self.post(
+            route=f"projects/{self.project_id}/ingest",
+            json=jcr,
+        )
+        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
+        self.logger.info(f"Logged info. Status: {status}", extra=jcr)
+
+    def log(self, ray_id, peephole, data):
+        # print(f"Logging Raymon Datatype...{type(data)}", flush=True)
+        jcr = self.structure(ray_id=ray_id, peephole=peephole, data=data.to_jcr())
+        self.logger.info(f"Logging data at {peephole}", extra=jcr)
+        resp = self.post(
+            route=f"projects/{self.project_id}/ingest",
+            json=jcr,
+        )
+        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
+        self.logger.info(f"Data logged at {peephole}. Status: {status}", extra=jcr)
+
+    def tag(self, ray_id, tags):
+        # TODO validate tags
+        jcr = self.structure(ray_id=ray_id, peephole=None, data=tags)
+        resp = self.post(
+            route=f"projects/{self.project_id}/rays/{ray_id}/tags",
+            json=tags,
+        )
+        status = "OK" if resp.ok else f"ERROR: {resp.status_code}"
+        self.logger.info(f"Ray tagged. Status: {status}", extra=jcr)
+
+    """HTTP METHODS"""
+
+    def post(self, route, json, params=None):
+        resp = self.session.post(
+            f"{self.url}/{route}",
+            json=json,
+            params=params,
+            headers=self.headers,
+        )
+        return resp
+
+    def put(self, route, json, params=None):
+        resp = self.session.put(
+            f"{self.url}/{route}",
+            json=json,
+            params=params,
+            headers=self.headers,
+        )
+        return resp
+
+    def get(self, route, json={}, params=None):
+        resp = self.session.get(
+            f"{self.url}/{route}",
+            json=json,
+            params=params,
+            headers=self.headers,
+        )
+        return resp
+
+    def delete(self, route, json={}, params=None):
+        resp = self.session.delete(
+            f"{self.url}/{route}",
+            json=json,
+            params=params,
+            headers=self.headers,
+        )
+
+        return resp
+
+    """API methods"""
+
+    def register_project(self, project_name):
+        project_data = {"project_name": project_name}
+        resp = self.post(route="projects", json=project_data)
+        return resp
+
+    def search_project(self, project_name):
+        project_data = {"project_name": project_name}
+        resp = self.get(route="projects/search", params=project_data)
+        return resp
+
+    def ls_projects(self):
+        resp = self.get(route="projects")
+        return resp
+
+    def add_org(self, org_id, description):
+        org = {"org_id": org_id, "description": description}
+        resp = self.post(route="orgs", json=org)
+        return resp
+
+    def get_org(self, org_id):
+        resp = self.get(route=f"orgs/{org_id}")
+        return resp
+
+    def add_user_to_org(self, org_id, user_id, user_readable):
+        org = {"user_id": user_id, "user_readable": user_readable}
+        resp = self.post(route=f"orgs/{org_id}/users", json=org)
+        return resp
+
+    def rm_user_from_org(self, org_id, user_id):
+        data = {"user_id": user_id}
+        resp = self.delete(route=f"orgs/{org_id}/users", json=data)
+        return resp
+
+    def add_m2mclient(self, project_id):
+        # org = {"user_id": user_id, "user_readable": user_readable}
+        resp = self.post(route=f"projects/{project_id}/m2m", json=None)
+        return resp
+
+    def get_m2mclient(self, project_id):
+        resp = self.get(route=f"projects/{project_id}/m2m", json=None)
+        return resp
+
+    def transfer_project(self, project_id, user_id, org_id):
+        # Either user_id or org_id should be None
+        owner = {"user_id": user_id, "org_id": org_id}
+        resp = self.put(route=f"projects/{project_id}", json=owner)
+        return resp
