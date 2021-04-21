6f314f88aec7c09ff81cbc6864f92dc66672f73d
Karel Vanhoorebeeck <karelvh@gmail.com>
2021-04-21 10:44:30 +0200

    Stashing things here. Not going to do this now.

--- raymon/__init__.py
+++ raymon/__init__.py
@@ -1,5 +1,8 @@
 from raymon.api import RaymonAPI
 from raymon.loggers import RaymonFileLogger, RaymonAPILogger
-from raymon.ray import Ray
+from raymon.trace import Trace
+from raymon.profiles.profile import DataProfile
+from raymon.profiles.watcher import FloatWatcher, IntWatcher, CategoricWatcher
+from raymon.profiles.stats import NumericStats, CategoricStats
 
 from .version import __version__
