c9d09b59ab5685d2bab3819c9df305c23f95ee13
Emre Ozan <emreozan@Emres-MacBook-Pro.local>
2021-06-17 09:27:13 +0200

    converted tocrh code into onnx code in anomaly.py

--- raymon/profiling/extractors/vision/anomaly.py
+++ raymon/profiling/extractors/vision/anomaly.py
@@ -1,70 +1,81 @@
+# https://github.com/onnx/tutorials#converting-to-onnx-format
+# https://pytorch.org/tutorials/advanced/super_resolution_with_onnxruntime.html
+
+import onnxruntime
+
 import numpy as np
-import torch
 import base64
-import torchvision.models as models
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
 from PIL import Image
+import os
 
 from raymon.profiling.extractors.structured.kmeans import KMeansOutlierScorer
-
-# Loosely based on "Deep Nearest Neighbor Anomaly Detection": https://arxiv.org/abs/2002.10445
-
-
-class ImageDataset(Dataset):
-    """Face Landmarks dataset."""
-
-    def __init__(self, loaded_data, transform=None):
-
-        self.loaded_data = loaded_data
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.loaded_data)
-
-    def __getitem__(self, idx):
-        sample = self.loaded_data[idx]
-        if self.transform:
-            sample = self.transform(sample)
-        return sample
-
 
 class DN2AnomalyScorer(KMeansOutlierScorer):
     def __init__(self, k=16, size=None, clusters=None, dist="euclidean"):
         super().__init__(k=k, clusters=clusters, dist=dist)
-        self.mobilenet = models.mobilenet_v2(pretrained=True).eval()
+        # model link - https://github.com/onnx/models/tree/master/vision/classification/mobilenet
+        self.mobilenet = onnxruntime.InferenceSession("raymon/models/mobilenetv2-7.onnx")
         self.size = size
-        tfs = [
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-        if size is not None:
-            tfs.append(
-                transforms.Resize(size=size),
-            )
-        self.tfs = transforms.Compose(tfs)
+                
+    def preprocess_data(self, pil_img):
+        # resize pil image 
+        img = pil_img.resize(size=(224, 224))
+        # Convert pil_image into numpy
+        numpy_img = np.array( img, dtype=np.float32 ) 
+        # normalize between 0.0 and 1.0
+        numpy_img = numpy_img / 255
+        # reshape image for onnx model
+        numpy_img = numpy_img.reshape( [1, 3, 224, 224] ) 
+        return numpy_img
+    
+    def preparation_batch_data(self, pil_img_list, batch_size):
+        # Our data is a list containing pil images
+        # Convert this image to numpy
+        # Make batches from data and store them in dataloader
+        dataloader = []
+        for i in range(0, len(pil_img_list), batch_size):
+            # if we arrive the end of the list, we have to adjust the shape of the array, 
+            if i+batch_size>len(pil_img_list):
+                batch_numpy = np.zeros((len(pil_img_list)-i, 3, 224, 224))
+            # Otherwise the shape of the array is (batch_size, 3, 224, 224).
+            else:
+                # Create zero numpy array with batch shape
+                batch_numpy = np.zeros((batch_size, 3, 224, 224))
+            # Make batches    
+            for k in range(i, i+batch_size):
+                # Avoid to exceed the length of the pil_img_list (list index out of range)
+                if k==len(pil_img_list):
+                    break
+                x = 0 
+                # Convert pil image and do other preprocessings
+                numpy_img = self.preprocess(pil_img_list[k])
+                batch_numpy[x, :, :, :] = numpy_img
+                x+=1 
+            dataloader.append(batch_numpy)
+        return dataloader
 
     def extract(self, data):
         if not isinstance(data, Image.Image):
-            raise ValueError(f"data must be of type PIL.Image.Image, not {data.shape}")
-        batchtf = self.tfs(data)[None, :]
-        feats = self.mobilenet(batchtf).detach().numpy()
-        return super().extract(data=feats)
+            raise ValueError(f"type of data must be PIL.Image.Image, not {data.shape}")
+        numpy_img = {self.mobilenet.get_inputs()[0].name: self.preprocess_data(data)}
+        feats = self.mobilenet.run(None, input_feed=numpy_img)
+        return super().extract(data=feats) 
 
-    def build(self, data, batch_size=16):
-        dataset = ImageDataset(loaded_data=data, transform=self.tfs)
-        # data is a list of images here
-        dataloader = DataLoader(dataset, shuffle=False, batch_size=batch_size, drop_last=True)
-        embeddings = torch.zeros(size=(len(data), 1000))
-        for batchidx, batchtf in enumerate(dataloader):
-            # batchtf = self.tfs(batch)
-            components = self.mobilenet(batchtf).detach()  # .numpy()
+    def build(self, data, batch_size=16): 
+        if not isinstance(data, list):
+            raise ValueError(f"type of the data must be list")
+        dataloader = self.preparation_batch_data(data, batch_size)               
+        embeddings = np.zeros((len(data), 1000))
+        for batchidx, batch_numpy in enumerate(dataloader):
+            batch_numpy = np.array( batch_numpy, dtype=np.float32 )
+            ort_inputs2 = {self.mobilenet.get_inputs()[0].name: batch_numpy}
+            components = self.mobilenet.run(None, input_feed=ort_inputs2)
             startidx = batchidx * batch_size
-            stopidx = startidx + len(components)
-            embeddings[startidx:stopidx, :] = components
+            stopidx = startidx + len(components[0])
+            embeddings[startidx:stopidx, :] = components[0]
 
-        embeddings = embeddings[embeddings.abs().sum(axis=1) != 0]
-        embeddings = embeddings.numpy()
+        indexes = np.where(np.sum(np.abs(embeddings), axis=1)!=0)
+        embeddings = embeddings[indexes]
         super().build(data=embeddings)
 
     """Serializable interface"""
