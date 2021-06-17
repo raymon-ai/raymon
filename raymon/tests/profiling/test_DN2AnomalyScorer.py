8579132db6f87160c59a58bc4d25ba9d709706d9
Emre Ozan <emreozan@Emres-MacBook-Pro.local>
2021-06-17 17:19:53 +0200

    add comment about building a model with model profile

--- raymon/tests/profiling/test_DN2AnomalyScorer.py
+++ raymon/tests/profiling/test_DN2AnomalyScorer.py
@@ -1,4 +1,4 @@
-#Import Libraries 
+#%%Import Libraries 
 import numpy as np
 import random
 from PIL import Image
@@ -18,6 +18,7 @@ from PIL import ImageEnhance
 def test_DN2AnomalyScorer():
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     ROOT = Path("..").resolve()
+    # test data contains 11 different images
     TEST_DATA_PATH = ROOT / "profiling/retinopathy_data/3"
     LIM = 500
     
@@ -33,7 +34,8 @@ def test_DN2AnomalyScorer():
             images.append(img)
             metadata.append([{"name": "srcfile", "value": fpath.stem, "type": "label"}])
         return images, metadata
-    
+
+    # Load model, you can see how the model build.
     MODEL_PATH = ROOT / "profiling/models/retinopathy@2.0.0.json"
     profile = ModelProfile().load(MODEL_PATH)
 
@@ -41,19 +43,28 @@ def test_DN2AnomalyScorer():
 
     for img in test_data:
         tags = profile.validate_input(img)
+        # I made each image blurred
         img_blur = img.copy().filter(ImageFilter.GaussianBlur(radius=3))
         tags_blur = profile.validate_input(img_blur)
+        # blurred image's value have to be bigger than normal image everytime.
         assert int(tags[0]['value']) < int(tags_blur[0]['value'])
 
 
+##### I build a model with 500 images and with my DN2AnomalyScorer class
+##### Create Profile class
+#profile = ModelProfile(
+#    name="retinopathy",
+#    version="2.0.0",
+#    components=[
+#        InputComponent(
+#            name="outlierscore",
+#            extractor=DN2AnomalyScorer(k=20, size=(256, 256)),
+#            dtype=DataType.FLOAT,
+#        )
+#    ]
+#)
+###### Save the model
+# profile.build(input=loaded_data)
+# fullprofile_path = Path("models/")
+# profile.save(fullprofile_path)
 
-
-
-
-
-
-
-
-
-
-# %%
